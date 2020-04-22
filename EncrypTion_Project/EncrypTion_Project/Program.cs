f584b5737e031167d7bb6fb2d678b23d6c8c038e
36m_901gs <njisom13@gmail.com>
2020-04-22 12:50:44 -0400

    Encode String Method
    Began method to use encoding path to encode string

--- EncrypTion_Project/EncrypTion_Project/Program.cs
+++ EncrypTion_Project/EncrypTion_Project/Program.cs
@@ -7,6 +7,38 @@ namespace EncrypTion_Project
 {
     class Encoder
     {
+        public List<char> encode_string(string message)
+        {
+            List<int> path = new List<int>();
+            string encoded_message;
+            double square_size = 0;
+
+            if(message.Length % 2 != 0)
+            {   
+                square_size = Math.Ceiling(Math.Sqrt((message.Length + 1)));
+
+            }
+            else
+            {
+                square_size = Math.Ceiling(Math.Sqrt(message.Length));
+
+            }
+
+            path = encoder((int)square_size);
+
+            //1. get the size of the square we'll need --done
+
+
+            //2. calculate the encode path -- done
+
+
+            //3. put the strings into the List
+
+            //4. return it as a string
+
+            return encoded_message;
+        }
+
         public List<int> encoder(int square_size) 
         {
             // int square_val = 0;
